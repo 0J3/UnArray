abf38e6eb21de8951ae0aa871ff1ffa8a76d8c18
0J3 <0J3@wafflepowered.com>
2021-06-09 10:16:14 +0200

    things

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 // Copyright (c) 2021 0J3. Licensed under the AGPL-3.0-or-later
 // Inefficient asf but idc
-const unArray = (arr)=>{
-	const array = []
-	arr.forEach((b)=>{
-		if(typeof b == typeof array) b.forEach((v)=>{
-			if (typeof v == typeof array) array.push(unArray(v))
-			else array.push(v)
-		})
-		else array.push(b)
-	})
-	return array
-}
+const unArray = (arr: any[]) => {
+  const array = [];
+  arr.forEach(b => {
+    if (typeof b == typeof array)
+      b.forEach((v: any) => {
+        if (typeof v == typeof array) array.push(unArray(v));
+        else array.push(v);
+      });
+    else array.push(b);
+  });
+  return array;
+};
 
-export default unArray
+export default unArray;
